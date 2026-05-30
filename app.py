081cc65ac83a68e51cfe2c47409edcb1fec1e843
Parmodkumar0126 <parmkumar6505@gmail.com>
2026-05-30 13:44:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,35 +13,24 @@ file_path = os.path.join(BASE_DIR, "nutrition.json")
 with open(file_path, "r") as f:
     nutrition_data = json.load(f)
 
-# Home route
 @app.route("/")
 def home():
     return "Food Analyzer API Running 🚀"
 
-# API route
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
     food = data.get("food", "").lower().strip()
 
-    matched_food = None
-
-    # Smart matching
-    for key in nutrition_data:
-        if food in key or key in food:
-            matched_food = key
-            break
-
-    if not matched_food:
+    # Exact match only
+    if food in nutrition_data:
         return jsonify({
-            "status": "not_found"
+            "status": "found",
+            "data": nutrition_data[food]
         })
 
-    result = nutrition_data[matched_food]
-
     return jsonify({
-        "status": "found",
-        "data": result
+        "status": "not_found"
     })
 
 if __name__ == "__main__":
